e311568ef2b36175194bcf04a131b58eee306dc0
hnjh0510 <gkstkdgns0510@naver.com>
2024-05-28 13:28:47 +0900

    시작화면

---
+++ Assets/scripts/button/Button.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void GameStartNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ToStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
