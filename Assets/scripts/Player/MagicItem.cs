45e439f2aebc18ada5608e3cfb85f46a576d6aba
Commonjh <wndghks999@naver.com>
2024-06-07 11:09:06 +0900

    아이템 형식만 구현

---
+++ Assets/scripts/Player/MagicItem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicItem : MonoBehaviour
+{
+    public enum MedicineType { food, downfullHPpostion, upfullHPpostion, downShotSpeedscroll, upShotSpeedscroll, downSpeedscroll, upSpeedscroll}
+    public MedicineType medicinetype;
+}
