eaf80872f7189e76cfdb6290a87c5837ac08d30b
hnjh0510 <gkstkdgns0510@naver.com>
2024-06-11 13:06:02 +0900

    상자 미완

---
+++ Assets/scripts/Map/Box.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public GameObject player;
+    public bool isOpen = false;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject == player && isOpen == false)
+        {
+            //公扁 靛而
+            Debug.Log("公扁 靛而");
+            isOpen = true;
+        }
+    }
+    /*void OnCollisionEnter2D(Collision2D collision1)
+    {
+        if (collision1.gameObject == player && isOpen == false)
+        {
+            //公扁 靛而
+            Debug.Log("公扁 靛而");
+            isOpen = true;
+        }
+    }*/
+}
